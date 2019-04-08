029dbf1ab6e646c3d71c861ccea3d82fd60d7ca1
Mikhail Terekhov <Mikhail.A.Terekhov@ya.ru>
2019-04-08 23:22:19 +0300

    Add Id.v proofs

--- src/Id.v
+++ src/Id.v
@@ -59,40 +59,107 @@ Ltac prove_with th :=
   try (constructor; assumption); congruence.
 
 Lemma lt_eq_lt_id_dec: forall (id1 id2 : id), {id1 i< id2} + {id1 = id2} + {id2 i< id1}.
-Proof. admit. Admitted.
+Proof.
+  intros [n1] [n2].
+  pose proof (lt_eq_lt_dec n1 n2) as [[H1 | H2] | H3].
+  - left. left. apply lt_conv. assumption.
+  - left. right. rewrite -> H2. reflexivity.
+  - right. apply lt_conv. assumption.
+Qed.
 
 Lemma gt_eq_gt_id_dec: forall (id1 id2 : id), {id1 i> id2} + {id1 = id2} + {id2 i> id1}.
-Proof. admit. Admitted.
+Proof.
+  prove_with gt_eq_gt_dec.
+  (* intros [n1] [n2].
+  pose proof (gt_eq_gt_dec n1 n2) as [[H1 | H2] | H3].
+  - right. apply gt_conv. assumption. 
+  - left. right. rewrite -> H2. reflexivity.
+  - left. left. apply gt_conv. assumption. *)
+Qed.
 
 Lemma le_gt_id_dec : forall id1 id2 : id, {id1 i<= id2} + {id1 i> id2}.
-Proof. admit. Admitted.
+Proof.
+  prove_with le_gt_dec.
+  (* intros [n1] [n2]. 
+  pose proof (le_gt_dec n1 n2) as [H1 | H2].
+  - left. apply le_conv. assumption.
+  - right. apply gt_conv. assumption. *)
+Qed.
 
 Lemma id_eq_dec : forall id1 id2 : id, {id1 = id2} + {id1 <> id2}.
-Proof. admit. Admitted.
+Proof.
+  prove_with Nat.eq_dec.
+  (* intros [n1] [n2].
+  pose proof (Nat.eq_dec n1 n2) as [H1 | H2].
+  - left. apply f_equal. assumption.
+  - right. intros Heq. inversion Heq as [Heq']. apply H2 in Heq'. assumption. *)
+Qed.
+
 
 Lemma eq_id : forall (T:Type) x (p q:T), (if id_eq_dec x x then p else q) = p.
-Proof. admit. Admitted.
+Proof.
+  intros T x p q.
+  destruct (id_eq_dec x x) as [| contra].
+  - reflexivity.
+  - contradiction.
+Qed.
 
 Lemma neq_id : forall (T:Type) x y (p q:T), x <> y -> (if id_eq_dec x y then p else q) = q.
-Proof. admit. Admitted.
+Proof.
+  intros T x y p q Hneq.
+  destruct (id_eq_dec x y) as [Heq | Hneq'].
+  - contradiction.
+  - reflexivity.
+Qed.
 
 Lemma lt_gt_id_false : forall id1 id2 : id,
     id1 i> id2 -> id2 i> id1 -> False.
-Proof. admit. Admitted.
+Proof.
+  intros [n1] [n2] H1 H2.
+  inversion H1.
+  inversion H2.
+  apply (gt_asym n1 n2).
+  assumption. assumption.
+Qed.
 
 Lemma le_gt_id_false : forall id1 id2 : id,
     id2 i<= id1 -> id2 i> id1 -> False.
-Proof. admit. Admitted.
+Proof.
+  intros [n1] [n2] Hle Hgt.
+  inversion Hle.
+  inversion Hgt.
+  apply (le_not_gt n2 n1).
+  assumption. assumption.
+Qed.
 
 Lemma le_lt_eq_id_dec : forall id1 id2 : id, 
     id1 i<= id2 -> {id1 = id2} + {id2 i> id1}.
-Proof. admit. Admitted.
+Proof. 
+  intros [n1] [n2] Hle.
+  pose proof (le_lt_eq_dec n1 n2) as [Hlt | Heq].
+  - inversion Hle. assumption.
+  - right. apply gt_conv. apply Hlt.
+  - left. apply f_equal. assumption.
+Qed.
 
 Lemma neq_lt_gt_id_dec : forall id1 id2 : id,
     id1 <> id2 -> {id1 i> id2} + {id2 i> id1}.
-Proof. admit. Admitted.
-    
+Proof.
+  intros [n1] [n2] Hne.
+  pose proof (gt_eq_gt_dec n1 n2) as [[H1 | H2] | H3].
+  - right. apply gt_conv. assumption.
+  - apply f_equal with (B:=id) (f:=Id) in H2.
+    apply Hne in H2. contradiction.
+  - left. apply gt_conv. assumption.
+Qed.
+
 Lemma eq_gt_id_false : forall id1 id2 : id,
     id1 = id2 -> id1 i> id2 -> False.
-Proof. admit. Admitted.
-
+Proof.
+  intros [n1] [n2] HeqId HgtId.
+  inversion HeqId as [Heq].
+  inversion HgtId as [n1e n2e Hgt Heq1 Heq2].
+  rewrite <- Heq in Hgt.
+  pose proof (gt_irrefl n1) as H.
+  contradiction.
+Qed.
