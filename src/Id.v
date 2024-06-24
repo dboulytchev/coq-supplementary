964ac2bd86f60ecb4e7e569213fbcdc7ff750318
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2024-06-25 01:03:54 +0300

    day 1

--- src/Id.v
+++ src/Id.v
@@ -12,15 +12,42 @@ Reserved Notation "m i<  n" (at level 70, no associativity).
 
 Inductive le_id : id -> id -> Prop :=
   le_conv : forall n m, n <= m -> (Id n) i<= (Id m)
-where "n i<= m" := (le_id n m).   
+where "n i<= m" := (le_id n m).
+
+Lemma le_id_le : forall n m, (Id n) i<= (Id m) -> n <= m.
+Proof.
+  intros n m H. inversion_clear H. assumption.
+Qed.
 
 Inductive lt_id : id -> id -> Prop :=
   lt_conv : forall n m, n < m -> (Id n) i< (Id m)
 where "n i< m" := (lt_id n m).   
 
+Lemma lt_id_lt : forall n m, (Id n) i< (Id m) -> n < m.
+Proof.
+  intros n m H. inversion_clear H. assumption.
+Qed.
+
 Inductive gt_id : id -> id -> Prop :=
   gt_conv : forall n m, n > m -> (Id n) i> (Id m)
-where "n i> m" := (gt_id n m).   
+where "n i> m" := (gt_id n m).
+
+Lemma gt_id_gt : forall n m, (Id n) i> (Id m) -> n > m.
+Proof.
+  intros n m H. inversion_clear H. assumption.
+Qed.
+
+Lemma eq_id_eq : forall n m, (Id n) = (Id m) <-> n = m.
+Proof.
+  intros n m. split; intros H.
+  - inversion H. reflexivity.
+  - subst. reflexivity.
+Qed.
+
+Lemma lt_gt_id_switch : forall id1 id2, id1 i< id2 <-> id2 i> id1.
+Proof.
+  intros id1 id2. destruct id1, id2. split; intros H; inversion_clear H; constructor; lia.
+Qed.
 
 Ltac prove_with th :=
   intros; 
@@ -64,30 +91,56 @@ Lemma le_gt_id_dec : forall id1 id2 : id, {id1 i<= id2} + {id1 i> id2}.
 Proof. prove_with le_gt_dec. Qed.
 
 Lemma id_eq_dec : forall id1 id2 : id, {id1 = id2} + {id1 <> id2}.
-Proof. admit. Admitted.
+Proof. prove_with Nat.eq_dec. Qed.
 
 Lemma eq_id : forall (T:Type) x (p q:T), (if id_eq_dec x x then p else q) = p.
-Proof. admit. Admitted.
+Proof.
+  intros T x p q.
+  destruct (id_eq_dec x x) as [Heq | Hneq].
+  { reflexivity. }
+  { exfalso. apply Hneq. reflexivity.  }
+Qed.
 
 Lemma neq_id : forall (T:Type) x y (p q:T), x <> y -> (if id_eq_dec x y then p else q) = q.
-Proof. admit. Admitted.
+Proof.
+  intros T x y p q. intros Hneq.
+  destruct (id_eq_dec x y) as [Heq | _].
+  * exfalso. apply Hneq. apply Heq.
+  * reflexivity.
+Qed.
+
+Ltac to_number_and_solve := intros id1 id2; destruct id1; destruct id2; intros H12 H21;
+  inversion_clear H12; inversion_clear H21; lia.
 
 Lemma lt_gt_id_false : forall id1 id2 : id,
     id1 i> id2 -> id2 i> id1 -> False.
-Proof. admit. Admitted.
+Proof. to_number_and_solve. Qed.
 
 Lemma le_gt_id_false : forall id1 id2 : id,
     id2 i<= id1 -> id2 i> id1 -> False.
-Proof. admit. Admitted.
+Proof. to_number_and_solve. Qed.
 
 Lemma le_lt_eq_id_dec : forall id1 id2 : id, 
     id1 i<= id2 -> {id1 = id2} + {id2 i> id1}.
-Proof. admit. Admitted.
+Proof.
+  intros id1 id2. intros H. destruct (lt_eq_lt_id_dec id1 id2) as [[Hlt12 | Heq] | Hlt21].
+  - right. apply lt_gt_id_switch. assumption.
+  - left. assumption.
+  - right. destruct id1, id2. inversion_clear H. inversion_clear Hlt21. lia.
+Qed.
 
 Lemma neq_lt_gt_id_dec : forall id1 id2 : id,
     id1 <> id2 -> {id1 i> id2} + {id2 i> id1}.
-Proof. admit. Admitted.
+Proof.
+  intros id1 id2 H. destruct (lt_eq_lt_id_dec id1 id2) as [[Hlt12 | Heq] | Hlt21].
+  - right. rewrite <- lt_gt_id_switch. assumption.
+  - contradiction.
+  - left. rewrite <- lt_gt_id_switch. assumption.
+Qed.
     
 Lemma eq_gt_id_false : forall id1 id2 : id,
     id1 = id2 -> id1 i> id2 -> False.
-Proof. admit. Admitted.
+Proof.
+  intros id1 id2. intros Heq. intros Hgt. subst.
+  destruct id2. apply gt_id_gt in Hgt. lia.
+Qed.
--- src/State.v
+++ src/State.v
@@ -4,7 +4,7 @@ Require Import List.
 Import ListNotations.
 Require Import Lia.
 Require Export Arith Arith.EqNat.
-Require Export Id.
+From semantics Require Export Id.
 
 From hahn Require Import HahnBase.
 
@@ -33,8 +33,8 @@ Section S.
     | [] => None
     end.
  
-  (* State a prove a lemma which claims that st_eval and
-     st_binds are actually define the same relation.
+  (* State and prove a lemma which claims that st_eval and
+     st_binds actually define the same relation.
    *)
 
   Lemma state_deterministic' (st : state) (x : id) (n m : option A)
@@ -49,30 +49,82 @@ Section S.
     (SN : st / x => n)
     (SM : st / x => m) :
     n = m. 
-  Proof. admit. Admitted.
+  Proof. induction SN as [|? ? ? ? ? ? ? IH].
+  { inversion SM.
+    - reflexivity.
+    - subst. contradiction. }
+  { inversion SM as [st2 id2 x2 H0 Hid'eq | st2 id2 x2 id'2 x'2 Hidneq Hevm].
+    - symmetry in Hid'eq. contradiction.
+    - subst. apply IH. apply Hevm. }
+  Qed.
   
   Lemma update_eq (st : state) (x : id) (n : A) :
     st [x <- n] / x => n.
-  Proof. admit. Admitted.
+  Proof. unfold update. apply st_binds_hd. Qed.
 
   Lemma update_neq (st : state) (x2 x1 : id) (n m : A)
         (NEQ : x2 <> x1) : st / x1 => m <-> st [x2 <- n] / x1 => m.
-  Proof. admit. Admitted.
+  Proof. unfold update. split; intros H.
+    { apply st_binds_tl with (st:=st) (x:=m) (id:=x1) (id':=x2) (x':=n).
+      - symmetry. apply NEQ.
+      - apply H. }
+    { inversion H.
+      - contradiction.
+      - apply H6. }
+  Qed.
+
+  Ltac imm_replace expr replacement :=
+    replace expr with replacement; [|reflexivity].
   
   Lemma update_shadow (st : state) (x1 x2 : id) (n1 n2 m : A) :
     st[x2 <- n1][x2 <- n2] / x1 => m <-> st[x2 <- n2] / x1 => m.
-  Proof. admit. Admitted.
+  Proof. split; unfold update; intros H.
+    { inversion H.
+      { subst. apply update_eq. }
+      { subst. apply st_binds_tl. apply H5.
+        rewrite update_neq with (st:=st) (x1:=x1) (x2:=x2) (m:=m) (n:=n1).
+        - unfold update. assumption.
+        - symmetry. assumption. } }
+    { inversion H.
+      { subst. apply update_eq. }
+      { subst. apply update_neq. auto.
+        imm_replace (st_binds ((x2, n1) :: st) x1 m) (st [x2 <- n1] / x1 => m).
+        rewrite <- update_neq; auto. } }
+  Qed.
   
   Lemma update_same (st : state) (x1 x2 : id) (n1 m : A)
         (SN : st / x1 => n1)
         (SM : st / x2 => m) :
     st [x1 <- n1] / x2 => m.
-  Proof. admit. Admitted.
+  Proof. destruct (id_eq_dec x1 x2).
+    { subst. apply state_deterministic with (n:=m) in SN; [|assumption]. rewrite <- SN.
+      apply update_eq. }
+    { rewrite <- update_neq; assumption. }
+(*inversion SN.
+    { subst. replace_def (((x1, n1) :: st0) [x1 <- n1]) ((st0) [x1 <- n1] [x1 <- n1]).
+      rewrite update_shadow. assumption. }
+    { rewrite H1. clear H1. subst x. *)
+  Qed.
+
+  Lemma binds_eq : forall st x n m, st [x <- n] / x => m -> n = m.
+  Proof.
+    intros. set (Hnm := state_deterministic _ _ _ _ (update_eq (st) x n) H).
+    assumption.
+  Qed.
   
   Lemma update_permute (st : state) (x1 x2 x3 : id) (n1 n2 m : A)
         (NEQ : x2 <> x1)
         (SM : st [x2 <- n1][x1 <- n2] / x3 => m) :
     st [x1 <- n2][x2 <- n1] / x3 => m.
-  Proof. admit. Admitted.
+  Proof.
+    destruct (id_eq_dec x3 x1) as [Heq31|Hneq31].
+      { subst. replace (update st x2 n1) with ((x2, n1)::st) in SM; [|reflexivity].
+        apply binds_eq in SM. apply update_neq; [assumption|]. subst. constructor. }
+      { destruct (id_eq_dec x3 x2) as [Heq32|Hneq32].
+        { subst. apply update_neq in SM; [|auto]. apply binds_eq in SM. subst.
+          apply update_eq. }
+        { repeat (apply update_neq; [auto|]).
+          repeat (rewrite <- update_neq in SM; [|auto]). assumption. } }
+  Qed.
 
 End S.
