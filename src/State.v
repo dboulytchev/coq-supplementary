ba4c2b4964a11f3642a391697ae432caa63f922f
Ilya Mikhaylov <ily2407@gmail.com>
2024-10-01 16:22:45 +0300

    implement state.v

--- src/State.v
+++ src/State.v
@@ -7,6 +7,7 @@ Require Export Arith Arith.EqNat.
 Require Export Id.
 
 From hahn Require Import HahnBase.
+Require Import Coq.Program.Equality.
 
 Section S.
 
@@ -49,30 +50,102 @@ Section S.
     (SN : st / x => n)
     (SM : st / x => m) :
     n = m. 
-  Proof. admit. Admitted.
+  Proof. 
+    induction SN.
+    - inversion SM.
+      + reflexivity. 
+      + subst. contradiction.
+    - inversion SM.
+      + subst. contradiction.
+      + subst. apply IHSN in H6. apply H6.
+  Qed.
   
   Lemma update_eq (st : state) (x : id) (n : A) :
     st [x <- n] / x => n.
-  Proof. admit. Admitted.
+  Proof.
+    constructor.
+  Qed.
 
   Lemma update_neq (st : state) (x2 x1 : id) (n m : A)
         (NEQ : x2 <> x1) : st / x1 => m <-> st [x2 <- n] / x1 => m.
-  Proof. admit. Admitted.
+  Proof. 
+    split.
+    - intros H0.
+      unfold update.
+      apply st_binds_tl.
+      + auto.
+      + assumption.
+    - intros H0.
+      unfold update in H0.
+      inversion H0.
+      + subst. contradiction.
+      + subst. assumption.
+  Qed.
   
   Lemma update_shadow (st : state) (x1 x2 : id) (n1 n2 m : A) :
     st[x2 <- n1][x2 <- n2] / x1 => m <-> st[x2 <- n2] / x1 => m.
-  Proof. admit. Admitted.
-  
+  Proof. 
+    split; intros; dependent destruction st; dependent destruction H.
+    - apply st_binds_hd.
+    - dependent destruction H0.
+      + contradiction.
+      + apply st_binds_tl.
+        * assumption.
+        * assumption.
+    - apply st_binds_hd.
+    - apply st_binds_tl.
+      + assumption.
+      + inversion H0; subst.
+        * contradiction.
+        * assumption.
+    - apply st_binds_hd.
+    - apply st_binds_tl.
+      + assumption.
+      + apply st_binds_tl.
+        * assumption.
+        * assumption.
+    - apply st_binds_hd.
+    - apply st_binds_tl.
+      + assumption.
+      + apply st_binds_tl.
+        * assumption.
+        * assumption.
+  Qed.
+
   Lemma update_same (st : state) (x1 x2 : id) (n1 m : A)
         (SN : st / x1 => n1)
         (SM : st / x2 => m) :
     st [x1 <- n1] / x2 => m.
-  Proof. admit. Admitted.
+  Proof. 
+    destruct (Id.id_eq_dec x1 x2).
+    - subst.
+      specialize (state_deterministic st x2 n1 m).
+      intros H0.
+      rewrite H0.
+      constructor.
+      + assumption.
+      + assumption.
+    - apply update_neq.
+      + assumption.
+      + assumption.
+  Qed.
   
   Lemma update_permute (st : state) (x1 x2 x3 : id) (n1 n2 m : A)
         (NEQ : x2 <> x1)
         (SM : st [x2 <- n1][x1 <- n2] / x3 => m) :
     st [x1 <- n2][x2 <- n1] / x3 => m.
-  Proof. admit. Admitted.
+  Proof.
+    inversion SM; subst.
+    - apply update_neq. 
+      + assumption.
+      + apply update_eq.
+    - inversion H5.
+      + apply update_eq.
+      + apply update_neq.
+        * apply not_eq_sym. assumption.
+        * apply update_neq.
+          ** apply not_eq_sym. assumption.
+          ** assumption.
+  Qed.
 
 End S.
